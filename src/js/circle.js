338fe03af3578dcc340ef5d5c90058a7853bf6b4
ssun <pros5018@nate.com>
2019-07-20 14:37:55 +0900

    second commit

---
+++ src/js/circle.js
@@ -0,0 +1,5 @@
+//CommonJS 스펙의 개별모듈화 => es6 named export
+export var area = (r) => Math.PI * r * r;
+
+export var circumference = (r) => 2 * Math.PI * r;
+
---
+++ src/js/cubic.js
@@ -0,0 +1,5 @@
+//commonJS 전체 모듈화 => es6 default export
+
+var cubic = (x) => x * x * x;
+
+export default cubic; //해당파일 전체를 export, default가 2번 나올리가 없음
